e358043834ed64d05ad3e41977f9b0670ef52389
Me <me@example.com>
2024-07-13 22:52:37 +0300

    Wrote test for GetFile function (files)

---
+++ files/get_test.go
@@ -0,0 +1,24 @@
+package files
+
+import (
+	"os"
+	"testing"
+)
+
+func TestGetFile(t *testing.T) {
+	temp := "temp"
+
+	t.Cleanup(func() {
+		os.Remove(temp)
+	})
+
+	file, err := GetFile(temp)
+
+	if err != nil {
+		t.Fail()
+	}
+
+	if file.Name() != temp {
+		t.Fail()
+	}
+}
