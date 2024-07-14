bb820f2e8baff8ac99ecf201994bde0597fbe7a0
Me <me@example.com>
2024-07-14 21:24:50 +0300

    Wrote test for DifferDirToCopy function (sync)

--- sync/differ_test.go
+++ sync/differ_test.go
@@ -1,8 +1,11 @@
 package sync
 
 import (
+	"os"
 	"slices"
 	"testing"
+
+	"github.com/ZXSQ1/syncdirs/files"
 )
 
 func TestDiffer(t *testing.T) {
@@ -56,5 +59,72 @@ func TestDiffer(t *testing.T) {
 		}, entry) {
 			t.Fail()
 		}
+	}
+}
+
+func TestDifferDirToCopy(t *testing.T) {
+	pathDirA := "temp1"
+	entriesDirA := []string{
+		"something/good",
+		"something/bad",
+		"something/to_eat/pizza",
+		"something/to_eat/tomato",
+		"something/to_drink/coffee",
+		"rust",
+	}
+
+	pathDirB := "temp2"
+	entriesDirB := []string{
+		"something/good",
+		"something/bad",
+		"something/to_eat/burger",
+		"something/to_eat/mango",
+		"something/to_eat/tomato",
+		"something/to_drink/tea",
+		"go",
+	}
+
+	t.Cleanup(func() {
+		os.RemoveAll(pathDirA)
+		os.RemoveAll(pathDirB)
+	})
+
+	for _, path := range entriesDirA {
+		path = pathDirA + "/" + path
+		os.MkdirAll(path, 0644)
+	}
+
+	for _, path := range entriesDirB {
+		path = pathDirB + "/" + path
+		os.MkdirAll(path, 0644)
+	}
+
+	source := make(chan string)
+	dest := make(chan string)
+
+	go DifferDirToCopy(pathDirA, pathDirB, source, dest)
+
+	for {
+		var sourcePath string
+		var destPath string
+
+		sourcePath, ok := <-source; 
+		if !ok {
+			break
+		}
+
+		destPath, ok = <-dest; 	
+		if !ok {
+			break
+		}
+
+		files.Copy(sourcePath, destPath)
+	}
+
+	contentsDirA, _ := files.ListDir(pathDirA, true)
+	contentsDirB, _ := files.ListDir(pathDirB, true)
+
+	if !slices.Equal(contentsDirA, contentsDirB) {
+		t.Fail()
 	}
 }
