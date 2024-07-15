80e76610d8b9a137fe1004b0fe970002c265555f
Me <me@example.com>
2024-07-15 17:00:08 +0300

    Implemented Bubbletea interface

--- ui/interface.go
+++ ui/interface.go
@@ -5,13 +5,20 @@ import (
 )
 
 type Model struct {
-	currentDirectory string
+	sourceDir string
+	destDir string
 	currentFile string
 	progress float32
 }
 
 func (m Model) Init() tea.Cmd {
-	return func() tea.Msg {
+	return nil
+}
 
-	}
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	return nil, nil
+}
+
+func (m Model) View() string {
+	return ""
 }
