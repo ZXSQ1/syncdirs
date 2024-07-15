c5a77ecdeadbea8ec0db7d026a061c68687baa2b
Me <me@example.com>
2024-07-15 13:54:56 +0300

    Wrote & Documented Synchronize function (sync)

--- sync/sync.go
+++ sync/sync.go
@@ -1,1 +1,43 @@
 package sync
+
+import (
+	"sync"
+
+	"github.com/ZXSQ1/syncdirs/files"
+)
+
+/*
+description: synchronizes 2 directories
+arguments:
+	- dirA: the string path to the first directory
+	- dirB: the string path to the second directory
+return: no return
+*/
+func Synchronize(dirA, dirB string) {
+	var waitGroup = &sync.WaitGroup{}
+
+	source := make(chan string)
+	dest := make(chan string)
+
+	go func() {
+		DifferDirToCopy(dirA, dirB, source, dest)
+	}()
+	
+	for {
+		sourcePath, sourceOk := <- source
+		destPath, destOk := <- dest
+
+		if !sourceOk || !destOk {
+			break
+		}
+		
+		waitGroup.Add(1)
+
+		go func() { 
+			files.Copy(sourcePath, destPath)
+			waitGroup.Done()
+		}()
+	}
+
+	waitGroup.Wait()
+}
