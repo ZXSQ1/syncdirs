d7636c806009dc4c990d10c0de3e6d2ab7f55671
Me <me@example.com>
2024-07-17 17:07:47 +0300

    Wrote and Documented Handle function (ui)

--- ui/handler.go
+++ ui/handler.go
@@ -3,6 +3,8 @@ package ui
 import (
 	"fmt"
 	"os"
+
+	"github.com/ZXSQ1/syncdirs/utils"
 )
 
 /*
@@ -15,4 +17,33 @@ func Help() {
 
 	fmt.Print(helpMsg)
 	os.Exit(1)
+}
+
+/*
+description: the function to handle the arguments and clean them
+arguments:
+  - fullArgs: the slice of the arguments that enter the program (including the program name)
+
+return: the slice of cleaned strings
+*/
+func Handle(fullArgs []string) []string {
+	args := fullArgs
+
+	if len(args) < 3 {
+		utils.PrintError("must specify at least 2 directories after\n")
+		Help()
+	}
+
+	args = args[1:]
+
+	for index, dir := range args {
+		if dir, ok := utils.ValidateDir(dir); ok {
+			args[index] = dir
+		} else {
+			utils.PrintError("invalid directory path\n")
+			Help()
+		}
+	}
+
+	return args
 }
