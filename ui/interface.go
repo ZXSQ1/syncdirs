e94f0f429b5e49923c639502df877cc7c463c5a9
Me <me@example.com>
2024-07-17 17:21:46 +0300

    Wrote Init method for Model (ui)

--- ui/interface.go
+++ ui/interface.go
@@ -1,1 +1,36 @@
 package ui
+
+import (
+	"github.com/ZXSQ1/syncdirs/sync"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+var dirs []string
+
+type Model struct {
+	sourceDir   string
+	destDir     string
+	currentFile string
+	progress    float32
+}
+
+func (m Model) Init() tea.Cmd {
+	var syncData = make(chan *sync.SyncData)
+	sync.SynchronizeMultiple(dirs, syncData)
+
+	return func() tea.Msg {
+		if data, ok := <-syncData; ok {
+			return data
+		}
+
+		return nil
+	}
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	return nil, nil
+}
+
+func (m Model) View() string {
+	return ""
+}
