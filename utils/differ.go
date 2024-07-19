84fbd857d3868a7df605559caaf47d88c85ba484
Me <me@example.com>
2024-07-19 23:52:38 +0300

    Removed DifferDirToCopy function (utils)

--- utils/differ.go
+++ utils/differ.go
@@ -1,8 +1,4 @@
-package sync
-
-import (
-	"github.com/ZXSQ1/syncdirs/files"
-)
+package utils
 
 type DifferenceTable struct {
 	Name    string
@@ -44,62 +40,6 @@ func Differ(tableA, tableB *DifferenceTable) {
 			tableB.Missing = append(tableB.Missing, key)
 		} else if val == onlyTableB {
 			tableA.Missing = append(tableA.Missing, key)
-		}
-	}
-}
-
-/*
-description: gets the difference in the contents of directories
-arguments:
-  - dirA: the path to the first directory
-  - dirB: the path to the second directory
-  - fileData: a channel to transfer the SyncDataFile structure
-  - dirData: a channel to transfer the *SyncDataDir structure
-
-return: no return
-*/
-func DifferDirToCopy(dirA, dirB string, fileData chan SyncDataFile, dirData chan SyncDataDir) {
-	entriesDirA, _ := files.ListDir(dirA, true)
-	tableDirA := &DifferenceTable{
-		Name:    dirA,
-		Entries: entriesDirA,
-	}
-
-	entriesDirB, _ := files.ListDir(dirB, true)
-	tableDirB := &DifferenceTable{
-		Name:    dirB,
-		Entries: entriesDirB,
-	}
-
-	Differ(tableDirA, tableDirB)
-
-	defer close(dirData)
-	defer close(fileData)
-
-	dirData <- SyncDataDir{
-		SourceDir:         dirA,
-		DestDir:           dirB,
-		SourceDirEntryLen: len(entriesDirA),
-		DestDirEntryLen:   len(entriesDirB),
-	}
-
-	for _, missingPath := range tableDirA.Missing {
-		sourcePath := tableDirB.Name + "/" + missingPath
-		destPath := tableDirA.Name + "/" + missingPath
-
-		fileData <- SyncDataFile{
-			SourceFile: sourcePath,
-			DestFile:   destPath,
-		}
-	}
-
-	for _, missingPath := range tableDirB.Missing {
-		sourcePath := tableDirA.Name + "/" + missingPath
-		destPath := tableDirB.Name + "/" + missingPath
-
-		fileData <- SyncDataFile{
-			SourceFile: sourcePath,
-			DestFile:   destPath,
 		}
 	}
 }
