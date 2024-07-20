a6fe07df289a4493405dd65836c1ab4ba87e1a17
Me <me@example.com>
2024-07-20 22:53:04 +0300

    Added and Documented NewLister function

--- app/lister.go
+++ app/lister.go
@@ -1,6 +1,19 @@
 package app
 
 type Lister struct {
-	dirNames   []string
-	dirEntries map[string]string
+	DirNames   []string
+	DirEntries map[string]string
+}
+
+/*
+description: returns a new lister instance
+arguments:
+  - dirNames: the string slice containing the directory names
+
+return: a Lister structure
+*/
+func NewLister(dirNames []string) Lister {
+	return Lister{
+		DirNames: dirNames,
+	}
 }
