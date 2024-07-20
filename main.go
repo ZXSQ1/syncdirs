4058f429396cabc00a6a9d8f99c0caf4d4ae56bf
Me <me@example.com>
2024-07-20 20:11:56 +0300

    Wrote main function

--- main.go
+++ main.go
@@ -4,8 +4,15 @@ import (
 	"os"
 
 	"github.com/ZXSQ1/syncdirs/ui"
+	"github.com/ZXSQ1/syncdirs/utils"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
-	ui.Dirs := ui.Handle(os.Args)
+	ui.Dirs = ui.Handle(os.Args)
+	program := tea.NewProgram(ui.Model{})
+
+	if _, err := program.Run(); err != nil {
+		utils.PrintError("error on running")
+	}
 }
