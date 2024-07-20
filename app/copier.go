6e2ccbf7ae19fb1fdd60594c3953f1a4e2a3c017
Me <me@example.com>
2024-07-21 01:58:56 +0300

    Wrote and Documented Add method (app/copier)

--- app/copier.go
+++ app/copier.go
@@ -13,3 +13,16 @@ return: the Copier instance
 func NewCopier() Copier {
 	return Copier{}
 }
+
+/*
+description: adds a source destination file pair
+arguments:
+  - sourceFile: the source file path to add
+  - destFile: the dest file path to add
+
+return: no return
+*/
+func (copier *Copier) Add(sourceFile, destFile string) {
+	copier.SourceFiles = append(copier.SourceFiles, sourceFile)
+	copier.DestFiles = append(copier.DestFiles, destFile)
+}
