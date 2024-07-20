8945b3b3f6baa750e4ed1f47dfa36fa4180ad094
Me <me@example.com>
2024-07-20 23:15:26 +0300

    Wrote and Documented NewPathDiffererAB function (app)

--- app/differer.go
+++ app/differer.go
@@ -8,3 +8,18 @@ type PathDiffererAB struct {
 	DirBName   string
 	Difference map[MissingPath]FoundPath
 }
+
+/*
+description: gets an instance of the PathDiffererAB structure
+arguments:
+  - dirA: the path of the first directory
+  - dirB: the path of the second directory
+
+return: a PathDiffererAB instance
+*/
+func NewPathDiffererAB(dirA, dirB string) PathDiffererAB {
+	return PathDiffererAB{
+		DirAName: dirA,
+		DirBName: dirB,
+	}
+}
