116c8e64c8f6d1b806015b68921a865974afe1ad
Me <me@example.com>
2024-07-24 21:05:47 +0300

    Documented Synchronize function (ui)

--- ui/start.go
+++ ui/start.go
@@ -2,6 +2,18 @@ package ui
 
 import "github.com/ZXSQ1/syncdirs/app"
 
+/*
+description: synchronizes 2 directories
+arguments:
+  - sourceDir: the string path of the source directory
+  - destDir: the string path of the destination directory
+  - sourceFile: the source file string channel
+  - destFile: the destination file string channel
+  - err: the error message string channel
+  - progress: the integer channel to contain the progress
+
+return: no return
+*/
 func Synchronize(sourceDir, destDir string, sourceFile, destFile, err chan string, progress chan int) {
 	lister := app.NewLister([]string{sourceDir, destDir})
 	lister.List()
