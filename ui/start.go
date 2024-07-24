c7b0b4cb739ee6f1744c86bf10e7a19054cdee41
Me <me@example.com>
2024-07-24 20:46:55 +0300

    Wrote Synchronize function (ui)

--- ui/start.go
+++ ui/start.go
@@ -1,1 +1,14 @@
 package ui
+
+import "github.com/ZXSQ1/syncdirs/app"
+
+func Synchronize(sourceDir, destDir string, sourceFile, destFile, err chan string, progress chan int) {
+	lister := app.NewLister([]string{sourceDir, destDir})
+	lister.List()
+
+	differer := app.NewPathDiffererAB(sourceDir, destDir, lister.Get(sourceDir), lister.Get(destDir))
+	differer.Differ()
+
+	copier := app.NewCopier(differer.GetFound(), differer.GetMissing())
+	copier.Copy(sourceFile, destFile, err, progress)
+}
