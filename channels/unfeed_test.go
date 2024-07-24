205a84bd5f3647ebd9cff1112b1322f3c7476fc2
Me <me@example.com>
2024-07-24 20:19:56 +0300

    Wrote test for Unfeed function (channels)

--- channels/unfeed_test.go
+++ channels/unfeed_test.go
@@ -1,1 +1,15 @@
 package channels
+
+import "testing"
+
+func TestUnfeed(t *testing.T) {
+	intChan := make(chan int)
+
+	go func() {
+		intChan <- 90
+	}()
+
+	if Unfeed(intChan) != 90 {
+		t.Fail()
+	}
+}
