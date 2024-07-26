b0ba5929b39e9b4f03e9c335297c53f99509febe
Me <me@example.com>
2024-07-27 00:49:32 +0300

    Made Synchronize function use float32 channel (ui)

--- ui/start.go
+++ ui/start.go
@@ -1,6 +1,9 @@
 package ui
 
-import "github.com/ZXSQ1/syncdirs/app"
+import (
+	"github.com/ZXSQ1/syncdirs/app"
+	"github.com/ZXSQ1/syncdirs/channels"
+)
 
 /*
 description: synchronizes 2 directories
@@ -10,17 +13,22 @@ arguments:
   - sourceFile: the source file string channel
   - destFile: the destination file string channel
   - err: the error message string channel
-  - progress: the integer channel to contain the progress
+  - progress: the float32 channel to contain the progress
 
 return: no return
 */
-func Synchronize(sourceDir, destDir string, sourceFile, destFile, err chan string, progress chan int) {
+func Synchronize(sourceDir, destDir string, sourceFile, destFile, err chan string, progress chan float32) {
 	lister := app.NewLister([]string{sourceDir, destDir})
 	lister.List()
 
 	differer := app.NewPathDiffererAB(sourceDir, destDir, lister.Get(sourceDir), lister.Get(destDir))
 	differer.Differ()
 
+	intProgress := make(chan int)
 	copier := app.NewCopier(differer.GetFound(), differer.GetMissing())
-	copier.Copy(sourceFile, destFile, err, progress)
+	copier.Copy(sourceFile, destFile, err, intProgress)
+
+	progressVal := float32((channels.Unfeed(intProgress)).(int) * 100 / len(lister.Get(sourceDir)))
+	channels.Feed(progress, progressVal)
+	channels.Close(progress)
 }
