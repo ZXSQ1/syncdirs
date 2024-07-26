64597c9044bb36262a96e9b02abcb2d706f1848e
Me <me@example.com>
2024-07-27 01:11:37 +0300

    Wrote Start function (ui)

--- main.go
+++ main.go
@@ -1,18 +1,9 @@
 package main
 
 import (
-	"os"
-
 	"github.com/ZXSQ1/syncdirs/ui"
-	"github.com/ZXSQ1/syncdirs/utils"
-	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
-	ui.Dirs = ui.Handle(os.Args)
-	program := tea.NewProgram(ui.Model{})
-
-	if _, err := program.Run(); err != nil {
-		utils.PrintError("error on running")
-	}
+	ui.Start()
 }
--- ui/start.go
+++ ui/start.go
@@ -1,8 +1,12 @@
 package ui
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/ZXSQ1/syncdirs/app"
 	"github.com/ZXSQ1/syncdirs/channels"
+	"github.com/ZXSQ1/syncdirs/utils"
 )
 
 /*
@@ -31,4 +35,44 @@ func Synchronize(sourceDir, destDir string, sourceFile, destFile, err chan strin
 	progressVal := float32((channels.Unfeed(intProgress)).(int) * 100 / len(lister.Get(sourceDir)))
 	channels.Feed(progress, progressVal)
 	channels.Close(progress)
+}
+
+func Start() {
+	dirs := Handle(os.Args)
+
+	sourceFile, destFile, err, progress := make(chan string), make(chan string), make(chan string), make(chan float32)
+
+	go func() {
+		for {
+			sourcePath := channels.Unfeed(sourceFile).(string)
+			destPath := channels.Unfeed(destFile).(string)
+			progressIndication := channels.Unfeed(progress).(float32)
+
+			fmt.Printf("%s -> %s (%.2f%%)\n", sourcePath, destPath, progressIndication)
+		}
+	}()
+
+	go func() {
+		for {
+			errMsg := channels.Unfeed(err).(string)
+			utils.PrintError(errMsg)
+		}
+	}()
+
+	switch len(dirs) {
+	case 0, 1:
+		utils.PrintError("not enough directories provided")
+		Help()
+	case 2:
+		Synchronize(dirs[0], dirs[1], sourceFile, destFile, err, progress)
+	default:
+		for i := 0; i < 2; i++ {
+			centralDir := dirs[0]
+
+			for _, syncDir := range dirs[1:] {
+				Synchronize(centralDir, syncDir, sourceFile, destFile, err, progress)
+			}
+		}
+	}
+
 }
