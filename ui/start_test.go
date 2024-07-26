86bea7474c73fa8f594478175a9edcfce9c7eea5
Me <me@example.com>
2024-07-27 00:35:36 +0300

    Wrote test for Synchronize function (ui)

--- ui/start_test.go
+++ ui/start_test.go
@@ -1,1 +1,54 @@
 package ui
+
+import (
+	"os"
+	"slices"
+	"testing"
+
+	"github.com/ZXSQ1/syncdirs/files"
+)
+
+func TestSynchronize(t *testing.T) {
+	dirA := "temp1"
+	dirB := "temp2"
+
+	dirAEntries := []string{
+		"nothing",
+		"everything",
+		"anything",
+	}
+
+	dirBEntries := []string{
+		"good",
+		"bad",
+		"evil",
+	}
+
+	t.Cleanup(func() {
+		os.RemoveAll(dirA)
+		os.RemoveAll(dirB)
+	})
+
+	for _, path := range dirAEntries {
+		path = dirA + "/" + path
+
+		fileObj, _ := files.GetFile(path, files.FilePerm)
+		fileObj.Close()
+	}
+
+	for _, path := range dirBEntries {
+		path = dirB + "/" + path
+
+		fileObj, _ := files.GetFile(path, files.FilePerm)
+		fileObj.Close()
+	}
+
+	Synchronize(dirA, dirB, nil, nil, nil, nil)
+
+	dirAContents, _ := files.ListDir(dirA, true)
+	dirBContents, _ := files.ListDir(dirB, true)
+
+	if !slices.Equal(dirAContents, dirBContents) {
+		t.Fail()
+	}
+}
